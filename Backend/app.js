fc854230694b7579e0e9125e0a8304c29ea31b64
TheWiXi <87704776+TheWiXi@users.noreply.github.com>
2024-08-28 07:01:00 -0500

    feat: :construction: commit para guardar cambios

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,9 @@ require("dotenv").config({ path: './config/.env' });
 const express = require('express');
 const connect =require ('./config/connection');
 const peliculaRoutes = require('./src/routes/pelicula');
+const salaRoutes = require('./src/routes/sala');
+const usuarioRoutes = require('./src/routes/usuario');
+// const boletoRoutes = require('./src/routes/boleto');
 
 const app = express();
 const port = process.env.API_PORT || 3000;
@@ -11,9 +14,14 @@ new connect();
 
 //middleware
 app.use(express.json());
+
 //common route
-app.use('/cineapi',peliculaRoutes)
+app.use('/cineapi',peliculaRoutes);
+app.use('/cineapi', salaRoutes);
+app.use('/cineapi', usuarioRoutes);
+// app.use('/cineapi/boletos', boletoRoutes);
+
 //start
 app.listen(port, () => {
-    console.log(`API listening at http://localhost:${port}`)
+    console.log(`API listening at http://localhost:${port}`);
 });
