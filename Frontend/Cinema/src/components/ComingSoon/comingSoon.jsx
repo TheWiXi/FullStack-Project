d17ad226d5f4d0d97ab65d60830bdef0a887c14e
TheWiXi <87704776+TheWiXi@users.noreply.github.com>
2024-08-29 09:43:05 -0500

    feat: :lipstick: cambios esteticos seccion proximamente

--- Frontend/Cinema/src/components/ComingSoon/comingSoon.jsx
+++ Frontend/Cinema/src/components/ComingSoon/comingSoon.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
 import './ComingSoon.css';
 
 const ComingSoon = () => {
@@ -23,14 +24,20 @@ const ComingSoon = () => {
         <a href="#" className="see-all">See all</a>
       </div>
       <div className="coming-soon">
-        {movies.map((movie, index) => (
-          <div className="movie-preview">
-            <img src={movie.gallery[0]} alt={movie.nombre}  className="preview-image" />
-            <div className="preview-info">
-              <h3 className="preview-title">{movie.nombre}</h3>
-              <p className="preview-genre">{movie.genero[0]}</p>
+        {movies.map((movie) => (
+          <Link 
+            to={`/movie/${movie._id}`} 
+            key={movie._id}  // <-- Usa el _id de la película como clave
+            className={`movie-preview`} // Puedes eliminar la clase 'active' si no la estás usando
+          >
+            <div className="movie-preview">
+              <img src={movie.gallery[0]} alt={movie.nombre}  className="preview-image" />
+              <div className="preview-info">
+                <h3 className="preview-title">{movie.nombre}</h3>
+                <p className="preview-genre">{movie.genero[0]}</p>
+              </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </>
